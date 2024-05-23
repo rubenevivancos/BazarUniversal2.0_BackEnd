9b2e6d155e7c8e71ece71d643fc234e584ba0ce3
rubenevivancos <rubenevivancos@hotmail.com>
2024-05-23 16:05:56 -0500

    Correccion en nombre de rura

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/routes.js');
+const routes = require('./Routes/routes.js');
 
 
 
